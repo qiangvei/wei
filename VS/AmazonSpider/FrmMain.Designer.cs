36eb4a9365b1cb59d4f035c27a3f4c2c0a4388a1
qiangvei <qiangvei@gmail.com>
2021-08-26 10:10:48 +0800

    a

---
+++ VS/AmazonSpider/FrmMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace AmazonSpider
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTxt = new System.Windows.Forms.TextBox();
+            this.fetchBtn = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tabContainer = new System.Windows.Forms.TabControl();
+            this.homePage = new System.Windows.Forms.TabPage();
+            this.fetchDataBtn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // urlTxt
+            // 
+            this.urlTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlTxt.Location = new System.Drawing.Point(2, 3);
+            this.urlTxt.Name = "urlTxt";
+            this.urlTxt.Size = new System.Drawing.Size(840, 21);
+            this.urlTxt.TabIndex = 0;
+            this.urlTxt.Text = "https://www.amazon.com/Fulfuloon-Swimming-Portable-Foldable-Bathing/product-revie" +
+    "ws/B012D2PT76/ref=cm_cr_arp_d_viewopt_srt?ie=UTF8&reviewerType=all_reviews&sortB" +
+    "y=recent&pageNumber=1";
+            this.urlTxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.urlTxt_KeyPress);
+            // 
+            // fetchBtn
+            // 
+            this.fetchBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fetchBtn.Location = new System.Drawing.Point(846, 2);
+            this.fetchBtn.Name = "fetchBtn";
+            this.fetchBtn.Size = new System.Drawing.Size(100, 23);
+            this.fetchBtn.TabIndex = 1;
+            this.fetchBtn.Text = "运行";
+            this.fetchBtn.UseVisualStyleBackColor = true;
+            this.fetchBtn.Click += new System.EventHandler(this.fetchBtn_Click);
+            // 
+            // tabContainer
+            // 
+            this.tabContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabContainer.Controls.Add(this.homePage);
+            this.tabContainer.Location = new System.Drawing.Point(3, 30);
+            this.tabContainer.Name = "tabContainer";
+            this.tabContainer.SelectedIndex = 0;
+            this.tabContainer.Size = new System.Drawing.Size(650, 732);
+            this.tabContainer.TabIndex = 2;
+            // 
+            // homePage
+            // 
+            this.homePage.Location = new System.Drawing.Point(4, 22);
+            this.homePage.Name = "homePage";
+            this.homePage.Padding = new System.Windows.Forms.Padding(3);
+            this.homePage.Size = new System.Drawing.Size(642, 706);
+            this.homePage.TabIndex = 0;
+            this.homePage.Text = "首页";
+            this.homePage.UseVisualStyleBackColor = true;
+            // 
+            // fetchDataBtn
+            // 
+            this.fetchDataBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fetchDataBtn.Location = new System.Drawing.Point(952, 2);
+            this.fetchDataBtn.Name = "fetchDataBtn";
+            this.fetchDataBtn.Size = new System.Drawing.Size(100, 23);
+            this.fetchDataBtn.TabIndex = 3;
+            this.fetchDataBtn.Text = "获取数据";
+            this.fetchDataBtn.UseVisualStyleBackColor = true;
+            this.fetchDataBtn.Click += new System.EventHandler(this.fetchDataBtn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(659, 52);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(393, 706);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 774);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.fetchDataBtn);
+            this.Controls.Add(this.tabContainer);
+            this.Controls.Add(this.fetchBtn);
+            this.Controls.Add(this.urlTxt);
+            this.Name = "FrmMain";
+            this.ShowIcon = false;
+            this.Text = "亚马逊爬虫";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.tabContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTxt;
+        private System.Windows.Forms.Button fetchBtn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TabControl tabContainer;
+        private System.Windows.Forms.TabPage homePage;
+        private System.Windows.Forms.Button fetchDataBtn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ VS/AmazonSpider/FrmMain.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace AmazonSpider
+{
+    public partial class FrmMain : Form
+    {
+        private delegate void UiHandler(object obj);
+        private ChromiumWebBrowser _browser = null;
+        public FrmMain()
+        {
+            InitializeComponent();
+            InitializeChromium();
+        }
+        /// <summary>
+        /// 初始化浏览器
+        /// </summary>
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            settings.Locale = "zh_CN";
+            settings.CachePath = Application.StartupPath+ @"\Cache";
+            settings.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36";
+            if (!Cef.IsInitialized) Cef.Initialize(settings);
+            //创建对象
+            string url = this.urlTxt.Text;
+            _browser = new ChromiumWebBrowser(url);
+            tabContainer.TabPages["homePage"].Controls.Add(_browser);
+            _browser.Dock = DockStyle.Fill;
+            //装载事件
+            ChromiumHandle();
+        }
+        public void ChromiumHandle() 
+        {
+            _browser.FrameLoadEnd += _browser_FrameLoadEnd;
+            _browser.LoadingStateChanged += _browser_LoadingStateChanged;
+        }
+        /// <summary>
+        /// 通过状态来判断加载完资源，实际上是不准确的，每个frame请求的状态变更都会引触发这个方法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _browser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            //Console.WriteLine("正在加载?:"+e.IsLoading.ToString()+" =>"+_browser.IsLoading.ToString());
+            //是否正在加载
+            if (e.IsLoading == false)
+            {
+
+            }
+        }
+
+        private void _browser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+            if (e.Frame.IsMain)
+            {
+                //_browser.ViewSource();
+                var task02 = _browser.GetMainFrame().GetSourceAsync();
+                task02.ContinueWith(t =>
+                {
+                    if (!t.IsFaulted)
+                    {
+                        //执行获取数据的方法
+                        fetchData();
+                    }
+                });
+            }
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void urlTxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                LoadUrl();
+            }
+        }
+        /// <summary>
+        /// 加载链接的页面 , 不传入链接则取链接输入框的值
+        /// </summary>
+        /// <param name="url"></param>
+        private void LoadUrl(string url=null)
+        {
+            if (url == null)
+            {
+                url = this.urlTxt.Text;
+            }
+            if (!string.IsNullOrEmpty(url))
+            {
+                _browser.Load(url);
+            }
+        }
+
+        private void fetchBtn_Click(object sender, EventArgs e)
+        {
+            LoadUrl();
+        }
+
+        private void fetchDataBtn_Click(object sender, EventArgs e)
+        {
+            fetchData();
+        }
+
+        private void fetchData()
+        {
+            try
+            {
+                string jsString = @"var reviewBox = document.getElementById('cm_cr-review_list');
+                                        for (var i = 0; i < reviewBox.childNodes.length; i++)
+                                            {
+                                                alert(reviewBox.childNodes[i].getElementsByClassName('a-profile'))
+                                            };
+                                    ";
+                jsString = @"(function () {
+                                function getScript(url, success) {
+                                    var script = document.createElement('script');
+                                    script.src = url;
+                                    var head = document.getElementsByTagName('head')[0],
+                                        done = false;
+                                    script.onload = script.onreadystatechange = function () {
+                                        if (!done && (!this.readyState
+                                            || this.readyState == 'loaded'
+                                            || this.readyState == 'complete')) {
+                                            done = true;
+                                            success();
+                                            script.onload = script.onreadystatechange = null;
+                                            head.removeChild(script);
+                                        }
+                                    };
+                                    head.appendChild(script);
+                                }
+                                getScript('https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js', function () {
+                                            if (typeof jQuery == 'undefined') {
+                                                console.log('Sorry, but jQuery wasn\'t able to load');
+                                            } else {
+                                                $(document).ready(function () {
+                                                    
+                                                });
+                                            }
+                                });
+})();
+";
+                _browser.GetMainFrame().ExecuteJavaScriptAsync(jsString);
+                Thread.Sleep(4000);
+                jsString = @"function getData(){   
+                                var data =[];
+                                $('div[data-hook=review]').each(function(i, item) {
+                                    var date = $(item).find('[data-hook=review-date]')[0].innerText;
+                                    var star = $(item).find('[data-hook=review-star-rating]')[0].innerText;
+                                    var title = $(item).find('[data-hook=review-title]')[0].innerText;
+                                    var body = $(item).find('[data-hook=review-body]')[0].innerText;
+                                    var userName = $(item).find('.a-profile-name')[0].innerText;
+                                    var userUrl = $($(item).find('.a-profile')[0]).attr('href');
+                                    data.push({
+                                        'date':date
+                                        ,'userName':userName
+                                        ,'userUrl':userUrl
+                                        ,'star':star
+                                        ,'title':title
+                                        ,'body':body
+                                        ,'host':document.domain
+                                    })
+                                })
+                                return data.length>0?JSON.stringify(data):'';
+            }getData(); ";
+                var task = _browser.GetMainFrame().EvaluateScriptAsync(jsString);
+                // 等待js 方法执行完后，获取返回值
+                task.ContinueWith(t =>
+                {
+                    if (t.Result.Result!=null)
+                    {
+                        var response = t.Result;
+                        string jsdata = response.Result.ToString();
+                        //MessageBox.Show(response.Result.ToString());
+                        //Console.WriteLine(resultStr);
+                        //richTextBoxAdd(resultStr);
+                        richTextBoxAdd(jsdata);
+                    }
+                });
+                //Console.WriteLine("Frame数量：" + _browser.GetBrowser().GetFrameCount());
+                //foreach(string n in _browser.GetBrowser().GetFrameNames())
+                //{
+                //    Console.WriteLine("Frame名："+n);
+                //}
+                //_browser.ViewSource();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void richTextBoxAdd(object obj)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.BeginInvoke(new UiHandler(richTextBoxAdd), obj);
+            }
+            else
+            {
+                this.richTextBox1.AppendText("\r\n=============\r\n" + obj.ToString());
+            }
+        }
+    }
+}
